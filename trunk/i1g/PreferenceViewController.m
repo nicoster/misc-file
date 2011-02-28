87659c136fd758451e2fc323a6a7dfe06e04ef33
nicoster <nicoster@d733c0c4-f2bb-11dd-802b-177316e4ac06>
2011-02-28 17:47:29 +0000

    settings UI is ready

--- trunk/i1g/PreferenceViewController.m
+++ trunk/i1g/PreferenceViewController.m
@@ -8,16 +8,24 @@
 #import "PreferenceViewController.h"
 
 @implementation PreferenceViewController
-@synthesize settings;
+@synthesize settings, login, viewLogin, viewSettings;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+	self.view.backgroundColor = [UIColor viewFlipsideBackgroundColor];
+  
 	// LLAMASETTINGS 3:  Make the connections to and from it
-	settings = [[LlamaSettings alloc] initWithPlist:@"mySettings.plist"];
+	settings = [[LlamaSettings alloc] initWithPlist:@"PreferenceSettings.plist"];
 	[settings setDelegate:self];
-	[(UITableView*)self.view setDataSource:settings];
-	[(UITableView*)self.view setDelegate:settings];
+	[viewSettings setDataSource:settings];
+	[viewSettings setDelegate:settings];
+//	viewSettings.hidden = YES;
+	
+	login = [[LlamaSettings alloc] initWithPlist:@"PreferenceLogin.plist"];
+	[login setDelegate:self];
+	[viewLogin setDataSource:login];
+	[viewLogin setDelegate:login];
+//	viewLogin.hidden = YES;
 }
 
 - (void) settingsChanged:(LlamaSettings *)ls
@@ -26,6 +34,23 @@
 
 - (void) buttonPressed:(NSString *)buttonKey inSettings:(LlamaSettings *)ls
 {
+	
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	[UIView beginAnimations:nil context:context];
+	[UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+	[UIView setAnimationDuration:1.0];
+	
+	[UIView setAnimationTransition: UIViewAnimationTransitionFlipFromLeft forView:self.view cache:YES];
+	NSInteger indexLogin = [[self.view subviews] indexOfObject: self.viewLogin];
+	NSInteger indexSettings = [[self.view subviews] indexOfObject: self.viewSettings];
+	//	NSInteger purple = [[whiteBackdrop subviews] indexOfObject:[whiteBackdrop viewWithTag:999]];
+	//	NSInteger maroon = [[whiteBackdrop subviews] indexOfObject:[whiteBackdrop viewWithTag:998]];
+	[self.view exchangeSubviewAtIndex:indexLogin withSubviewAtIndex:indexSettings];
+	//	
+	//	[UIView setAnimationDelegate:self];
+	[UIView setAnimationDidStopSelector:@selector(animationFinished:)];
+	[UIView commitAnimations];	
+	 	
 }
 
 - (void) dealloc
