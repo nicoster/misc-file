4dcd7db4beaa84c0abebed8d8ef3bcd10fb2d0da
nicoster <nicoster@d733c0c4-f2bb-11dd-802b-177316e4ac06>
2011-07-10 11:27:22 +0000

    rename some files

--- OrderDishes/src/org/mfn/dishes/MainActivity.java
+++ OrderDishes/src/org/mfn/dishes/MainActivity.java
@@ -5,13 +5,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.GridView;
 
-public class OrderDishes3Activity extends Activity {
+public class MainActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.grid_2);
+		setContentView(R.layout.main);
 
 		GridView g = (GridView) findViewById(R.id.myGrid);
 		g.setAdapter(new DishesAdapter(this));
