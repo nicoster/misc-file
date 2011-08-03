0805c074b2a0bfa8bfbd3500400f139e0ecf2a7c
qianyong325@gmail.com <qianyong325@gmail.com@d733c0c4-f2bb-11dd-802b-177316e4ac06>
2011-08-03 09:25:10 +0000

    goback

--- wapgroup/src/org/mfn/wap/group/WapPageActivity.java
+++ wapgroup/src/org/mfn/wap/group/WapPageActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.http.SslError;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.HttpAuthHandler;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
 
 public class WapPageActivity extends Activity {
 	
@@ -46,8 +50,36 @@ public class WapPageActivity extends Activity {
 	        		  super.onReceivedError(view, errorCode, description, failingUrl);
 	        	}
 	        	
+	        	public void onPageFinished(WebView view, String url)
+	        	{
+	        		Log.d("test","test");
+	        	}
+	        	
 	        };
 	        
 	        wv.setWebViewClient(wvc);
+	        
+	        View goBack = (ImageView)findViewById(R.id.goBackButton);
+	        View goForward = (ImageView)findViewById(R.id.goForwardButton);
+	        
+			goBack.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					if(wv.canGoBack())
+					{
+						wv.goBack();
+					}
+				}
+			});
+			
+			goForward.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					if(wv.canGoForward())
+					{
+						wv.goForward();
+					}
+				}
+			});
+	        
 	 }
+
 }
