9cea70cd590baff152b9ace79ae92e2d32a6eae1
frey.wong@gmail.com <frey.wong@gmail.com@d733c0c4-f2bb-11dd-802b-177316e4ac06>
2011-08-08 03:04:10 +0000

    support download and update

--- wapgroup/src/org/mfn/wap/group/Constants.java
+++ wapgroup/src/org/mfn/wap/group/Constants.java
@@ -1,6 +1,10 @@
 package org.mfn.wap.group;
 
 public class Constants {
+	
 	public static final String PARAM_WAP_URL = "param_wap_url";
 	public static final String PARAM_WAP_TITLE = "param_wap_title";
+	
+	public static final String CONFIG_APP_STATUS_URL = "http://www.sz.js.cn/3g/aclient/version.txt";
+	
 }
