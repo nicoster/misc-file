b7137b9552b62a0632dcd40efa19056ad826e260
nicoster <nicoster@d733c0c4-f2bb-11dd-802b-177316e4ac06>
2012-07-18 07:53:07 +0000

    add bind to function.cpp

--- vm/function.cpp
+++ vm/function.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 #include "boost/function.hpp"
-
+#include "boost/bind.hpp"
 bool some_func(int i,double d) {
   return i>d;
 }
 
-int main() {
-  boost::function<bool (int,double)> f;
-  f=&some_func;
-  f(10,1.1);
-}
+// int main() {
+//   boost::function<bool (int,double)> f;
+//   f=&some_func;
+//   std::cout << f(10,1.1) << std::endl;
+// 	return 0;
+// }
 
+using namespace boost;
+struct demo_class
+{
+	int add(int a, int b){
+		return a+b;
+	}
+};
+
+int main(){
+	demo_class sc;
+	function<int (int, int)> func1;
+	function<int (int, int)> func2;
+	
+	func1 = bind(&demo_class::add, &sc,_1,_2);
+	func2 = &demo_class::add;
+//	std::cout << func1(10,20) << func2(&sc,10, 20) << std::endl;
+}
